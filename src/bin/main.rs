95f580eac9ad4b4ee28b4de2e9beb59480d28ba5
Steven Lotito <steven.lotito@gmail.com>
2026-01-03 16:29:17 -0800

    initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,59 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::timer::timg::TimerGroup;
+use esp_println::println;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.1.0
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    // Define the LED pin - we're going to connect it to GPIO pin 1
+    let mut led = Output::new(peripherals.GPIO1, Level::High, OutputConfig::default());
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        // Turn on LED
+        led.set_high();
+        // Wait for 1 second
+        println!("hello blinky");
+        Timer::after(Duration::from_millis(1_000)).await;
+        // Turn off LED
+        led.set_low();
+        // Wait for a second
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
